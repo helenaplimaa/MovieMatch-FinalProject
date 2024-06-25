0ba19f0afcc8bdb4e2958c0c3885eec45f147cc8
Helena Lima <helena.limaa148@gmail.com>
2024-06-25 14:27:39 -0300

    Initial Commit

---
+++ MovieMatch-FinalProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieMatch-FinalProject
+//
+//  Created by Isabel Pereira Lima on 25/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieMatch-FinalProject/MovieMatch_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieMatch_FinalProjectApp.swift
+//  MovieMatch-FinalProject
+//
+//  Created by Isabel Pereira Lima on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieMatch_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
